ba24c0317fc702e26f7293993de627807c229821
South_Sea <tksz0613@gmail.com>
2025-06-17 15:56:59 +0900

    settings

---
+++ backend-spring/src/main/java/com/Project03/backendspring/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.Project03.backendspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class TestController {
+    @GetMapping("/api/hello")
+    public Map<String, String> hello() {
+        return Map.of("message", "spring 백엔드에서 보낸 메시지입니다");
+    }
+}
