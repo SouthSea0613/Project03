5d00bfd2ad52ec9aed78fb3465f1d765581064d6
South_Sea <tksz0613@gmail.com>
2025-06-17 17:00:26 +0900

    fastapi ready to use

--- backend-fastapi/crud.py
+++ backend-fastapi/crud.py
@@ -1,10 +1,2 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from . import models, schemas
-
-async def create_item(db: AsyncSession, item: schemas.ItemCreate):
-    # Pydantic 모델을 SQLAlchemy 모델로 변환
-    db_item = models.Item(name=item.name, description=item.description)
-    db.add(db_item)
-    await db.commit()
-    await db.refresh(db_item)
-    return db_item
