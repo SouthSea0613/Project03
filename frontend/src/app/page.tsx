aaf8adec3c45dad74ce75c66608c1b73334f1263
South_Sea <tksz0613@gmail.com>
2025-06-17 15:02:29 +0900

    전역설정

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [message, setMessage] = useState('메시지를 불러오는 중...');
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/hello') // Proxy 설정으로 인해 localhost:8080/api/hello로 요청됨
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/hello`) // Proxy 설정으로 인해 localhost:8080/api/hello로 요청됨
         .then((res) => res.json())
         .then((data) => {
           setMessage(data.message);
@@ -19,9 +19,9 @@ export default function Home() {
 
   return (
       <main className="flex min-h-screen flex-col items-center justify-center p-24">
-        <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
-          <p className="text-2xl">{message}</p>
-        </div>
+          <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+              <p className="text-2xl">{message}</p>
+          </div>
       </main>
   );
 }
