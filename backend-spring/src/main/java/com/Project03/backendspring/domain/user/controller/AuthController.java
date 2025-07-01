d4d09a11093cadf2fb626afce9d6cf19a30bc851
alsdud0301 <jm931017@gmail.com>
2025-07-01 14:02:26 +0900

    로그인 토큰 유효성 확인

--- backend-spring/src/main/java/com/Project03/backendspring/domain/user/controller/AuthController.java
+++ backend-spring/src/main/java/com/Project03/backendspring/domain/user/controller/AuthController.java
@@ -1,11 +1,15 @@
 package com.Project03.backendspring.domain.user.controller;
 
-import com.Project03.backendspring.domain.user.dto.AuthRequestDto;
+import com.Project03.backendspring.common.dto.MessageDto;
+import com.Project03.backendspring.domain.user.dto.request.LoginDto;
+import com.Project03.backendspring.domain.user.dto.request.SignupDto;
 import com.Project03.backendspring.domain.user.service.AuthService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,17 +20,21 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody AuthRequestDto.Signup requestDto) {
+    public ResponseEntity<MessageDto> signup(@RequestBody SignupDto signUpDto) {
         try {
-            authService.signup(requestDto);
-            return ResponseEntity.status(HttpStatus.CREATED).body("회원가입 성공");
+            if(authService.signup(signUpDto)) {
+                return ResponseEntity.status(HttpStatus.CREATED).body(new MessageDto(true, "회원가입 성공"));
+            }
+            else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageDto(false, "잘못된 요청"));
+            }
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new MessageDto(false, "통신 에러"));
         }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody AuthRequestDto.Login requestDto, HttpServletResponse response) {
+    public ResponseEntity<String> login(@RequestBody LoginDto requestDto, HttpServletResponse response) {
         try {
             String token = authService.login(requestDto);
             response.addHeader("Authorization", token);
@@ -34,5 +42,11 @@ public class AuthController {
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }
+    }
+    @PostMapping("/user/me")
+    public ResponseEntity<MessageDto> getUserInfo(@AuthenticationPrincipal UserDetails userDetails){
+//        String username = userDetails.getUsername();
+//        SignupRequestDto user = userDetails.getUsername()
+        return ResponseEntity.ok(new MessageDto(true,"유효한 토큰 & user 정보 조회"));
     }
 }
--- backend-spring/src/main/java/com/Project03/backendspring/domain/user/service/AuthService.java
+++ backend-spring/src/main/java/com/Project03/backendspring/domain/user/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.Project03.backendspring.domain.user.service;
 
-import com.Project03.backendspring.domain.user.dto.AuthRequestDto;
+import com.Project03.backendspring.domain.user.dto.request.LoginDto;
+import com.Project03.backendspring.domain.user.dto.request.SignupDto;
 import com.Project03.backendspring.domain.user.entity.User;
 import com.Project03.backendspring.domain.user.entity.UserRole;
 import com.Project03.backendspring.domain.user.repository.UserRepository;
@@ -19,31 +20,36 @@ public class AuthService {
     private final JwtUtil jwtUtil;
 
     @Transactional
-    public void signup(AuthRequestDto.Signup requestDto) {
-        if (userRepository.existsByUsername(requestDto.getUsername())) {
-            throw new IllegalArgumentException("이미 사용 중인 아이디입니다.");
+    public boolean signup(SignupDto requestDto) {
+        try {
+            if (userRepository.existsByUsername(requestDto.getUsername())) {
+                return false;
+            }
+            if (userRepository.existsByEmail(requestDto.getEmail())) {
+                return false;
+            }
+
+            String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
+
+            User user = User.builder()
+                    .username(requestDto.getUsername())
+                    .password(encodedPassword)
+                    .name(requestDto.getName())
+                    .email(requestDto.getEmail())
+                    .postcode(requestDto.getPostcode())
+                    .address(requestDto.getAddress())
+                    .detailAddress(requestDto.getDetailAddress())
+                    .userRole(UserRole.USER) // 기본 역할은 USER
+                    .build();
+            userRepository.save(user);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
-        if (userRepository.existsByEmail(requestDto.getEmail())) {
-            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
-        }
 
-        String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
-
-        User user = User.builder()
-                .username(requestDto.getUsername())
-                .password(encodedPassword)
-                .name(requestDto.getName())
-                .email(requestDto.getEmail())
-                .postcode(requestDto.getPostcode())
-                .address(requestDto.getAddress())
-                .detailAddress(requestDto.getDetailAddress())
-                .userRole(UserRole.USER) // 기본 역할은 USER
-                .build();
-
-        userRepository.save(user);
     }
 
-    public String login(AuthRequestDto.Login requestDto) {
+    public String login(LoginDto requestDto) {
         User user = userRepository.findByUsername(requestDto.getUsername())
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
