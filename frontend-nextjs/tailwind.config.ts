cdba42c5a33a678a7a18f76178ae540010c9edd0
alsdud0301 <jm931017@gmail.com>
2025-07-04 00:26:33 +0900

    tailwind.config.ts

---
+++ frontend-nextjs/tailwind.config.ts
@@ -0,0 +1,14 @@
+
+import type { Config } from 'tailwindcss'
+
+export default {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                myColor: '#000000',
+            },
+        },
+    },
+    plugins: [],
+} satisfies Config
